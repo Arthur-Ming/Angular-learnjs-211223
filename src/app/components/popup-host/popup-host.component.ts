398d19aa493d48e7020d8a174439f0e64fe10ab2
Arthur Ming <arthurming7@gmail.com>
2024-01-21 08:35:23 +0300

    feat: add popup-host component

--- src/app/components/popup-host/popup-host.component.ts
+++ src/app/components/popup-host/popup-host.component.ts
@@ -7,16 +7,26 @@ import {Component, Input, TemplateRef, ViewChild, ViewContainerRef} from '@angul
 })
 export class PopupHostComponent {
     @Input() set template(template: TemplateRef<unknown> | null) {
-        if (this.popupContentViewContainerRef) {
-            this.popupContentViewContainerRef.clear();
-            template && this.popupContentViewContainerRef.createEmbeddedView(template);
-        }
+        this.clearView();
+        this.createView(template);
     }
 
     @ViewChild('popupContent', {read: ViewContainerRef, static: true})
     private readonly popupContentViewContainerRef: ViewContainerRef | null = null;
 
-    isHidden() {
-        return !this.popupContentViewContainerRef?.length;
+    clearView(): void {
+        if (this.popupContentViewContainerRef) {
+            this.popupContentViewContainerRef.clear();
+            this.isHidden = true;
+        }
     }
+
+    createView(template: TemplateRef<unknown> | null): void {
+        if (template && this.popupContentViewContainerRef) {
+            this.isHidden = false;
+            this.popupContentViewContainerRef.createEmbeddedView(template);
+        }
+    }
+
+    isHidden = true;
 }
